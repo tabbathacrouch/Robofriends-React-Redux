2dfa77d75b48ea7d3f0728b1a4c87c800ba9747e
Tabbatha Crouch <tabbathacrouch@gmail.com>
2021-06-14 17:45:02 -0500

    add width and height properties to images

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,12 @@ import React from "react";
 const Card = ({ name, email, id }) => {
   return (
     <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
-      <img alt="" src={`https://robohash.org/${id}?size=200x200`} />
+      <img
+        alt=""
+        src={`https://robohash.org/${id}?size=200x200`}
+        width="200px"
+        height="200px"
+      />
       <div>
         <h2>{name}</h2>
         <p>{email}</p>
