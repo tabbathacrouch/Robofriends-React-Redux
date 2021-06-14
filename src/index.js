bde082aefbee5337aac5c38bfaa8de459f7b13e6
Tabbatha Crouch <tabbathacrouch@gmail.com>
2021-06-14 17:20:25 -0500

    pwa

--- src/index.js
+++ src/index.js
@@ -7,7 +7,8 @@ import { createLogger } from "redux-logger";
 import App from "./containers/App";
 import "./style/index.css";
 import "tachyons";
-import * as serviceWorker from "./serviceWorker";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import reportWebVitals from "./reportWebVitals";
 
 import { searchRobots, requestRobots } from "./reducers";
 
@@ -25,7 +26,9 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
+serviceWorkerRegistration.register();
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
